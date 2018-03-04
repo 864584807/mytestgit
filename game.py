c2bd09095ab0eb280ab433d0147ca31f0a7c1abe
864584807 <864584807@qq.com>
2018-03-04 20:16:14 +0800

    fixed a bug

--- game.py
+++ game.py
@@ -4,12 +4,12 @@ import random
 
 a=random.randint(1,100)
 b=input("please input a number from 1 to 100:")
-i=0
+i=1
 while a!=b:
 	if a>b:
-		b=input("your number is small，please input again:")
+		b=input("your number is small，please input again:\n")
 	else:
-		b=input("your number is big ,please input again:")
-	i=1+1
+		b=input("your number is big ,please input again\n")
+	i=i+1
 else:
 	print"congratulations,you have input %d times"% i		
